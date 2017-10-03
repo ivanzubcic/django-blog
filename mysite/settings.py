c36c89538a71e150d357eed2a74667e0b5c44767
Ivan Zubcic <ivanzub@gmail.com>
2017-10-03 11:25:53 +0200

    static dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+"""STATICFILES_DIRS = (
+    os.path.join('blog/static'),
+)"""
 
 LOGIN_REDIRECT_URL = '/'
 
