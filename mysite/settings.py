159cf8202b64a0d3717209fce5f1ae4cbfb7dc2f
Ivan Zubcic <ivanzub@gmail.com>
2017-10-03 14:27:19 +0200

    db conf

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,9 +83,8 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-"""import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)"""
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
